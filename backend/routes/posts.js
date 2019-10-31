40955de1422f8443db1e932f042f215ba8b06985
carlos.cotelo <cclafuente@gmail.com>
2019-10-31 15:59:31 +0100

    rutas en distinto archivo

---
+++ backend/routes/posts.js
@@ -0,0 +1,64 @@
+const express = require("express");
+
+const router = express.router();
+
+router.post('/api/posts', (req, res, next) => {
+    console.log('llega el post al servidor');
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    console.log(" post to add " + post);
+    post.save().then(savedPost => {
+        res.status(201).json({
+             message: ' Post added succesfully ', 
+             postId : savedPost._id
+        });
+    });
+});
+//TeQsUh3j0CT8NeTv
+//cclafuente user
+
+router.put('/api/posts/:id', (req, res, next) => {
+    console.log('llega llamada a servidor');
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    console.log(" post to update " + post);
+    Post.updateOne({_id: req.params.id}, post).then(updatedPost => {
+        res.status(201).json({
+             message: ' Post updated succesfully ', 
+             postId : updatedPost._id
+        });
+    });
+});
+
+router.get('/api/posts', (req, res, next) => {
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: ' enviado correctamente',
+            posts : documents
+        });
+    });
+});
+
+router.get('/api/posts/:id', (req, res, next) => {
+   Post.findById(req.params.id).then(result => {
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: "Post not found"});
+        }
+   })
+});
+
+router.delete('/api/posts/:id', (req, res, next) => {
+    Post.deleteOne({_id: req.params.id}).then(result => {
+        res.status(200).json({ 
+            message : " Post deleted "
+        });
+    });
+});
