f93255479fc8f41fd8545f2734412a7bfe927d3e
Carlos CCOL. Cotelo Lafuente <carlos.cotelo@stkproj.com>
2019-11-12 13:43:53 +0100

    check auth

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Post = require("../models/post");
 const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
@@ -27,6 +28,7 @@ const storage = multer.diskStorage({
 });
 
 router.post("", 
+    checkAuth,
     multer({storage: storage}).single("image"), 
     (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
@@ -50,6 +52,7 @@ router.post("",
 });
 
 router.put('/:id', 
+    checkAuth,
     multer({storage: storage}).single("image"), 
     (req, res, next) => {
         let imagePath;
@@ -111,7 +114,9 @@ router.get('/:id', (req, res, next) => {
    })
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', 
+    checkAuth,
+    (req, res, next) => {
     Post.deleteOne({_id: req.params.id}).then(result => {
         res.status(200).json({ 
             message : " Post deleted "
